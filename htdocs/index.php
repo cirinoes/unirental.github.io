521653cac3aaa08714e14368a69a93abebc5a6ef
cirinoes <alastairjayden@gmail.com>
2026-01-05 13:19:23 +0800

    Create index.php

---
+++ htdocs/index.php
@@ -0,0 +1,10 @@
+<?php include __DIR__ . '/_partials/header.php'; ?>
+<div class="card">
+  <h1>UniRental</h1>
+  <p>Browse by category, search, add items to cart, and checkout.</p>
+  <p>
+    <a class="btn-nav" href="/vehicles.php">Browse Items</a>
+    <a class="btn-nav" href="/signup.php">Member Registration</a>
+  </p>
+</div>
+<?php include __DIR__ . '/_partials/footer.php'; ?>
